907c24849734ea4758826a41de36d360d7e5f362
Luzzle <49113582+Luzzle@users.noreply.github.com>
2025-02-18 07:42:49 +1100

    Updated makefile for platform detection and sample SDL code to test build environment

--- makefile
+++ makefile
@@ -7,10 +7,26 @@ SRC_FILES := *.cpp
 
 C_FILES := $(addprefix $(SRC_DIR), $(SRC_FILES))
 
+ifeq ($(OS), Windows_NT)
+# add build dependencies for windows
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		CFLAGS += -I/opt/homebrew/include -L/opt/homebrew/lib -lSDL2
+	endif
+
+	ifeq ($(UNAME_S),Linux)
+# add build dependencies for linux
+	endif
+endif
+
 all: $(TARGET)
 
 $(TARGET): $(MAIN)
 	$(CC) $(CFLAGS) -o $(TARGET) $(C_FILES)
+
+run: $(TARGET)
+	@$(TARGET)
 
 clean: $(TARGET)
 	rm $(TARGET)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
+#include <SDL2/SDL.h>
+
 #include <iostream>
 
+
 int main(void){
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    bool keep_window_open = true;
+    while(keep_window_open)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e) > 0)
+        {
+            switch(e.type)
+            {
+                case SDL_QUIT:
+                    keep_window_open = false;
+                    break;
+            }
+    
+            SDL_UpdateWindowSurface(window);
+        }
+    }
 }
